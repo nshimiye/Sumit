568ee09fed0842c054a42eed1923a08a6d85dee6
Marcellin Nshimiyimana <mn2587@columbia.edu>
2014-06-08 00:06:34 -0400

    cleanup again

--- app/client/helpers/handlebars.js
+++
@@ -1,9 +0,0 @@
-//Handlebars.registerHelper creates a global 
-//helper that can be user within any template
-UI.registerHelper('pluralize', function(n, thing) {
-    if (n === 1) {
-        return '1 ' + thing;
-    } else {
-        return n + ' ' + thing + 's';
-    }
-});
--- app/client/main.js
+++
@@ -1,5 +0,0 @@
-Meteor.startup(function() {
-    Deps.autorun(function() {
-        console.log('There are ' + Posts.find().count() + ' posts');
-    });
-});
